70867ce950fc53a73a16a6c76d4b6fab2487e5cc
Erik Karlgren Domercq <erik.karlgren@gmail.com>
2022-09-18 15:01:40 +0200

    Added fish plugins and aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
+
+    # Aliases
     alias ip 'ip -c'
-    alias ls 'lsd'
+    alias ls lsd
     alias grep rg
+    alias code codium
+    alias vim nvim
+    alias e nvim
 end
